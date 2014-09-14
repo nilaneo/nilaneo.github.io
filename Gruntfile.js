61a3636fe214750a1940d6863bf0515a1bf84323
nilaneo <shlikhtenkoneonila@gmail.com>
2014-09-14 21:33:44 +0300

    add grunt and sass

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'sass',
+          cssDir: 'css'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['compass'],
+        options: {
+          spawn: false,
+          livereload: true
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          livereload: true
+        }
+      }
+    }
+    
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('default', ['compass', 'connect', 'watch']);
+
+};
